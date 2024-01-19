47a8adef558fd07e5cc2307ee817a275bf434cab
jongithub59 <jdelrealj01@gmail.com>
2024-01-19 14:32:11 -0600

    basic algorithm created

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//App Logic
+
+//create new project class/object with project name and tasks array
+
+//add new project to an array that will be later displayed to the DOM
+
+//create new task class/object with title, description, priority, and due date
+//collected from the user and add to the task array
+
+//user selects their tasks for their created projects to add, edit or delete
+
+//organize all tasks based on when they are due, if they are important, and if 
+//they are completed
+
+
+
+//DOM Logic
+
+//display all projects from project array in the main section
+
+//add project button that calls new project class to make project
+
+//add task button that calls new task class to create a task
+
+//all tasks section that displays all task array elements from all projects
+
+//other tabs that display tasks from all projects based on some criteria
+
+
