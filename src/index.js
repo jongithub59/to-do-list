b9a918fc6b35b01136102f00e680329fea755317
jongithub59 <jdelrealj01@gmail.com>
2024-01-29 17:13:40 -0600

    addTask modules complete and minor reworks

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createProject, createToDo } from "./modules/createToDo"
 
 
 const projects = []
+const tasks = []
 
   
 // projects.push(createProject('To do list'))
@@ -18,23 +19,40 @@ const projects = []
 //DOM Logic
 const screenController = function() {
     const projectList = document.querySelector('.project-list')
+    const main = document.querySelector('.to-do-content')
+
+    const openDialog = function(button, dialog) {
+        button.addEventListener('click', (event) => {
+            event.preventDefault()
+            dialog.showModal()
+        })
+    }
+    
+    const cancelDialog = function(button, input, dialog) {
+        button.addEventListener('click', (event) => {
+            event.preventDefault()
+            resetDialog(input, dialog)
+        })
+    }
+    
+    const resetDialog = function(input, dialog) {
+        input.value = ''
+        dialog.close()
+    }
     
     
     //add project button that calls new project class to make project
     const addNewProject = function() {
         
         const dialog = document.getElementById('project-dialog')
-        const projectNameInput = document.getElementById('project-name')
+        const nameInput = document.getElementById('project-name')
         
         const createButtons = function() {
             const submit = document.getElementById('project-submit')
             const addProject = document.querySelector('.add-project')
             const cancel = document.getElementById('project-cancel')
             
-            addProject.addEventListener('click', (event) => {
-                event.preventDefault()
-                dialog.showModal()
-            })
+            openDialog(addProject, dialog)
             
             submit.addEventListener('click', (event) => {
                 event.preventDefault()
@@ -42,14 +60,12 @@ const screenController = function() {
                 dialog.close()
             })
             
-            cancel.addEventListener('click', (event) => {
-                event.preventDefault()
-                resetDialog()
-            })
+            cancelDialog(cancel, nameInput, dialog)
+
         }
         
         const submitProject = function() {
-            let projectName = projectNameInput.value
+            let projectName = nameInput.value
             
             if (projectName == '') return alert('Project Name cannot be empty')
             
@@ -64,13 +80,10 @@ const screenController = function() {
             project.textContent = projectName
 
             projectList.appendChild(project)
+            resetDialog(nameInput, dialog)
         }
         
 
-        const resetDialog = function() {
-            projectNameInput.value = ''
-            dialog.close()
-        }
 
         createButtons()
     }
@@ -78,23 +91,90 @@ const screenController = function() {
     //add task button that calls new task class to create a task
     const addNewTask = function() {
         const dialog = document.getElementById('task-dialog')
-        const addTask = document.getElementById('add-task')
-        const cancel= document.getElementById('task-cancel')
+        const title = document.getElementById('task-title')
+        const desc = document.getElementById('task-desc')
+        const date = document.getElementById('task-date')
+        
+        const createButtons = function() {
+            const addTask = document.getElementById('add-task')
+            const cancel = document.getElementById('task-cancel')
+            const submit = document.getElementById('task-submit')
 
-        addTask.addEventListener('click', (event) => {
-            event.preventDefault()
-            dialog.showModal()
-        })
+            openDialog(addTask, dialog)
+    
+            cancel.addEventListener('click', (event) => {
+                event.preventDefault()
+                console.log('yess')
+                resetDialog()
+            })
+    
+            submit.addEventListener('click', (event) => {
+                event.preventDefault()
+                submitTask()
+                dialog.close()
+            })
 
-        cancel.addEventListener('click', (event) => {
-            event.preventDefault()
-            dialog.close()
-        })
+            const submitTask = function() {
+                // if (title.value == '' || desc.value == '' || date.value == '') return alert('Text fields must be filled')
+                console.log(title)
+                console.log()
 
-        const resetDialog = function() {
-            projectNameInput.value = ''
-            dialog.close()
+                const taskPriority = document.querySelector("input[name='task-priority']:checked").value
+                
+                tasks.push(createToDo(title, desc, taskPriority, date))
+                displayTask(title, desc, taskPriority, date)
+            }
+
+            const displayTask= function(title, desc, taskPriority, date) {
+                const toDo = document.createElement('div')
+                const check = document.createElement('img')
+                const name = document.createElement('p')
+                const priority = document.createElement('p')
+                const dueDate = document.createElement('p')
+                const edit = document.createElement('div')
+                const icon = document.createElement('img')
+                const remove = document.createElement('div')
+                const icon2 = document.createElement('img')
+
+                toDo.classList.add('to-do')
+                check.src = './pics/check icon.png'
+                check.alt ='checkbox'
+                name.classList.add('to-do-name')
+                name.textContent = title.value
+
+                priority.classList.add('priority')
+                priority.textContent = 'Priority'
+                if (taskPriority == 'low') priority.classList.add('low-priority')
+                if (taskPriority == 'medium') priority.classList.add('medium-priority')
+                if (taskPriority == 'high') priority.classList.add('high-priority')
+
+                dueDate.classList.add('date')
+                dueDate.textContent = date.value
+                edit.classList.add('edit-button')
+                icon.src = './pics/square-edit-outline.svg'
+                edit.appendChild(icon)
+                remove.classList.add('delete-button')
+                icon2.src = './pics/trash-can-outline.svg'
+                remove.appendChild(icon2)
+
+                toDo.appendChild(check)
+                toDo.appendChild(name)
+                toDo.appendChild(priority)
+                toDo.appendChild(dueDate)
+                toDo.appendChild(edit)
+                toDo.appendChild(remove)
+                main.appendChild(toDo)
+            }
+            //need module that will add a task specifically to the project currently in
+
+    
+            const resetDialog = function() {
+                dialog.close()
+            }
+
         }
+
+        createButtons()
     }
     
     //all tasks section that displays all task array elements from all projects
